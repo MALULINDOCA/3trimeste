0ce05fe530ef472ca9fec55fc0e8027f4911da04
MALULINDOCA <160866551+MALULINDOCA@users.noreply.github.com>
2024-10-29 08:55:19 -0300

    29/10

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 function criaCartao(categoria,pergunta,resposta){
-    let container = document.getElementById('container')
-    let article = document.createElement('article')
-    article.className = 'cartao'
-    article.innerHTML = 'isso é um teste'
-    container.appendChild(article)
+   let container = document.getElementById('container')
+   let article = document.createElement('article')
+   article.className = 'cartao'
 
+   article.innerHTML = `
+   <div class="cartao-conteudo">
+
+   <h3> ${categoria} </h3>
+
+   <div class="cartao-conteudo-pergunta"
+   <p> ${pergunta} </p>
+   </div>
+
+   <div class="cartao-conteudo-resposta">
+   <p> ${resposta} </p>
+   </div>
+   </div>
+   `
+    container.appendChild (article)
 }
 
 criaCartao("Mateamatica","quanto é 2+2","4")
+criaCartao("Arte","azul com amarelo vira qual cor?", "verde")
